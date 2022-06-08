42660f91787924bebc0e2654c10b47d48cc6e748
kimanheng <59006202+kimanheng@users.noreply.github.com>
2022-06-08 09:48:57 +0700

    update

---
+++ javascript/cursor.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+    
+    var mouseX = 0, mouseY = 0;
+    var xp = 0, yp = 0;
+
+    $(document).mousemove(function(e) {
+        mouseX = e.pageX - 30;
+        mouseY = e.pageY - 30;
+    });
+    
+    setInterval(function() {
+        xp += ((mouseX - xp)/6);
+        yp += ((mouseY - yp)/6);
+        $("#cursor").css({left: xp + 'px', top: yp + "px"});
+    }, 20);
+});
