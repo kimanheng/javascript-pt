944bb5aec7083a4d9179f9ee55cef7d737c10cf1
kimanheng <59006202+kimanheng@users.noreply.github.com>
2022-06-08 14:53:57 +0700

    update

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -1,16 +1,9 @@
-jQuery(document).ready(function() {
-    
-    var mouseX = 0, mouseY = 0;
-    var xp = 0, yp = 0;
-
-    $(document).mousemove(function(e) {
-        mouseX = e.pageX - 30;
-        mouseY = e.pageY - 30;
-    });
-    
-    setInterval(function() {
-        xp += ((mouseX - xp)/6);
-        yp += ((mouseY - yp)/6);
-        $("#cursor").css({left: xp + 'px', top: yp + "px"});
-    }, 20);
+var cursorfollow = document.getElementById("cursor-follow");
+var cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function(e) {
+    cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
+    cursorfollow.style.left = e.clientX + "px",
+    cursorfollow.style.top = e.clientY + "px";
 });
+
